3f962ce7a27be63330d6d64d6508097956ac5365
unknown <s.adeboye@outlook.com>
2023-03-20 19:05:52 +0000

    adding db info

--- web-Scrapper.py
+++ web-Scrapper.py
@@ -2,3 +2,13 @@ import pandas as pd # library used for working with dataset (analyzing, cleaning
 import requests   # library that handles the HTTP calls
 from bs4 import BeautifulSoup # library that is use for parsing HTML and XML documents
 from sqlalchemy import create_engine
+
+
+# A simple web scrapping application to pull data from a web page
+
+# Database credentials ( Note : never display your db opening like this in your main code)
+db_user_name = 'postgres'
+db_password = 'root'
+host = 'localhost'
+port = 5432
+db_name = 'universityDB'
