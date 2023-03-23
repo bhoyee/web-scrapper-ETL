c2d8f530e1d7cabc848d340ed8b9e3317e97ab88
unknown <s.adeboye@outlook.com>
2023-03-23 21:38:13 +0000

    adding data transform layer

--- webScrapper-v2.py
+++ webScrapper-v2.py
@@ -20,3 +20,11 @@ def extract_data():
     df = pd.read_html(html_data)[0]
     df.to_csv('Data-file/wikipedia_data_v2_england.csv', index= False)
     print('Data Successfully written a csv file')
+
+    
+# Data load transform and load layer
+def transform_load_to_db():
+    data = pd.read_csv('data/wikipedia_data_v2_england.csv') # Read csv file
+    engine = create_engine(f'postgresql+psycopg2://{db_user_name}:{db_password}@{host}:{port}/{db_name}')
+    data.to_sql('university_england_data_v2', con= engine, if_exists='replace', index= False)
+    print('Data successfully written to PostgreSQL database')
